d075a46695f9071dee429e06e9b8662da28f1c45
morgr <morgr98@gmail.com>
2022-06-20 14:31:52 +0000

    part 1 +2

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,1 +1,164 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct MallocMetadata_t{
+    size_t size;
+    bool is_free;
+    MallocMetadata_t* next;
+    MallocMetadata_t* prev;
+}MallocMetadata;
+
+
+size_t num_free_blocks = 0;
+size_t num_free_bytes = 0;
+size_t num_allocated_blocks = 0;
+size_t num_allocated_bytes = 0;
+size_t num_meta_data_bytes = 0;
+size_t size_meta_data = sizeof(MallocMetadata);
+
+
+MallocMetadata* allocated_list = NULL;
+
+static MallocMetadata* addBlock(MallocMetadata* last_alloced, size_t size)
+{
+    MallocMetadata* new_block;
+    void* p_ret = sbrk(size+size_meta_data);
+    if(p_ret == (void*)(-1))
+        return NULL;
+    new_block = (MallocMetadata*)p_ret;
+    if (last_alloced!=NULL)
+    {
+        last_alloced->next = new_block;
+    }
+    new_block->size = size;
+    new_block->is_free = false;
+    new_block->prev = last_alloced;
+    num_allocated_blocks++;
+    num_allocated_bytes+=size;
+    num_meta_data_bytes+=size_meta_data;
+    return new_block;
+}
+
+static MallocMetadata* findFreeBlock(size_t size, MallocMetadata** last_alloced)
+{
+    MallocMetadata* iterator = allocated_list;
+    while (iterator!=NULL)
+    {
+        if(iterator->is_free == true && iterator->size >= size)
+            return iterator;
+        last_alloced = &iterator;
+        iterator=iterator->next;
+    }
+    return iterator;
+}
+void* smalloc(size_t size)
+{
+
+    if((size <= 0) || size > 100000000 )
+        return NULL;
+
+    MallocMetadata* last_alloced_block = NULL;
+    MallocMetadata* block_to_use = findFreeBlock(size, &last_alloced_block);
+    if(block_to_use == NULL)
+    {
+        block_to_use = addBlock(last_alloced_block, size);
+    }
+    else {
+        block_to_use->is_free = false;
+        num_free_blocks--;
+        num_free_bytes -= block_to_use->size;
+    }
+    return (void*)(block_to_use+1);
+}
+
+void* scalloc(size_t num, size_t size)
+{
+    if(size <= 0 || num <= 0)
+        return NULL;
+    size_t total_size= size*num;
+    if(total_size > 10000000)
+        return NULL;
+    void* ret_pointer = smalloc(total_size);
+    memset(ret_pointer, 0, total_size);
+    return ret_pointer;
+
+}
+
+void sfree(void* p)
+{
+    if(p == NULL)
+    {
+        return;
+    }
+
+    MallocMetadata* pointer =(MallocMetadata*)(p);
+    pointer = pointer -1;
+    if(pointer->is_free == true)
+    {
+        return;
+    }
+    pointer->is_free = true;
+    num_free_blocks++;
+    num_free_bytes+= pointer->size;
+}
+
+void* srealloc(void* oldp , size_t size)
+{
+    if(size <= 0 || size > 100000000)
+        return NULL;
+    if(oldp == NULL)
+    {
+        return smalloc(size);
+    }
+    MallocMetadata* pointer =(MallocMetadata*)(oldp);
+    pointer = pointer -1;
+    if(pointer->size >= size)
+    {
+        return oldp;
+    }
+
+    void* newp = smalloc(size);
+    if(newp == NULL) {
+        return NULL;
+    }
+    memmove(newp , oldp , pointer->size);
+    sfree(oldp);
+    return newp;
+}
+
+
+size_t _num_free_blocks(){
+    return num_free_blocks;
+}
+
+size_t _num_free_bytes(){
+    return num_free_bytes;
+}
+
+size_t _num_allocated_blocks()
+{
+    return num_allocated_blocks;
+}
+
+size_t _num_allocated_bytes(){
+    return num_allocated_bytes;
+}
+
+size_t _num_meta_data_bytes()
+{
+    return num_meta_data_bytes;
+}
+
+size_t _size_meta_data(){
+    return size_meta_data;
+}
+
+
+
+
+
+
+
 
