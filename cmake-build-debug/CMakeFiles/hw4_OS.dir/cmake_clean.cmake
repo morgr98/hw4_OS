237623c67ec34cc36188c580af231f7666923860
giladgafny1 <giladgafny@campus.technion.ac.il>
2022-06-22 10:22:32 +0000

    shits

---
+++ cmake-build-debug/CMakeFiles/hw4_OS.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw4_OS.dir/main3test.cpp.o"
+  "CMakeFiles/hw4_OS.dir/main3test.cpp.o.d"
+  "hw4_OS"
+  "hw4_OS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw4_OS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/hw4_OS.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hw4_OS.
+# This may be replaced when dependencies are built.
